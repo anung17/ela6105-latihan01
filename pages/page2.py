cfcfd6e51dc0d7c95840a6a77467c5753c972ded
Anung B Ariwibowo <barliant@gmail.com>
2025-11-21 19:13:25 +0700

    Menambahkan file page1, page2, page3.

---
+++ pages/page2.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyleft © 2025 Anung <anung@trisakti.ac.id>
+# 2025-11-18 10:20
+# Distributed under terms of the MIT license.
+
+"""
+Exampe from
+https://msaqib-genai.medium.com/how-to-create-a-multi-page-app-in-streamlit-7f788e715db4
+"""
+
+import streamlit as st
+import plotly.express as px
+import numpy as np
+import matplotlib.pyplot as plt
+
+st.title("Data Visualization")
+
+# Generate some data
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+# Plot data
+fig, ax = plt.subplots()
+ax.plot(x, y)
+# Display the plot
+st.pyplot(fig)
+
+
+
---
+++ pages/page3.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyleft © 2025 Anung <anung@trisakti.ac.id>
+# 2025-11-18 10:20
+# Distributed under terms of the MIT license.
+
+"""
+Exampe from
+https://msaqib-genai.medium.com/how-to-create-a-multi-page-app-in-streamlit-7f788e715db4
+"""
+
+import streamlit as st
+
+st.title("Settings")
+st.write("This page allows you to customize your preferences.")
+
+
+
