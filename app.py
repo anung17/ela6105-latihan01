6c497eb8e053155f31b54491ead9d2644b0b46ba
Anung B Ariwibowo <barliant@gmail.com>
2025-11-22 16:46:56 +0700

    Menambahkan file app.

---
+++ app.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyleft © 2025 Anung <anung@trisakti.ac.id>
+# 2025-11-18 10:18
+# Distributed under terms of the MIT license.
+
+"""
+Example from
+https://msaqib-genai.medium.com/how-to-create-a-multi-page-app-in-streamlit-7f788e715db4
+"""
+
+import streamlit as st
+
+pages = [
+    st.Page(page="pages/page1.py", title="Home", icon="🏠"),
+    st.Page(page="pages/page2.py", title="Visualisasi Data", icon="📊"),
+    st.Page(page="pages/page3.py", title="Settings", icon="⚙️")
+]
+
+pg = st.navigation(
+    pages,
+    position="sidebar",
+    expanded=True
+)
+
+pg.run()
